e7f67b0f24d447d1de5a1e355a56e49b3107bc0a
codewithrutuja23 <rutuja1536@gmail.com>
2026-08-13 13:10:31 +0530

    Add files via upload

---
+++ script (2).js
@@ -0,0 +1,9 @@
+function startLearning() {
+    document.getElementById("courses").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+function startCourse() {
+    window.location.href = "certificate.html";
+}
